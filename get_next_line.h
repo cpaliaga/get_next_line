0dd752d047055c4f6827872d6c722fb085b03921
Cristina Patricia Aliaga Jimenez <caliaga-@c3r6s3.42madrid.com>
2022-12-13 19:17:24 +0100

    cabeceras

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caliaga- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/13 18:55:03 by caliaga-          #+#    #+#             */
+/*   Updated: 2022/12/13 19:13:10 by caliaga-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -6,20 +18,10 @@
 
 size_t	ft_strlen(char *str);
 char	*ft_strchr(char *s, int c);
-
-char			*ft_strjoin(char *s1, char *s2);
-
-/**
- *  * @brief Reads a file from a file descriptor
- *   *
- *    * @param fd file descriptor to read from
- *     * @return char* a line read from a file descriptor, if NULL: nothing else to
- *      * read or error
- *       */
-char			*get_next_line(int fd);
-
-char			*ft_get_line(char *save);
-char			*ft_save(char *save);
-char			*ft_read_and_save(int fd, char *save);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_get_line(char *save);
+char	*ft_save(char *save);
+char	*ft_read_and_save(int fd, char *save);
 
 #endif
